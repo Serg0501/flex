c17df6a158c63af81038c8ca2ed73ce4a743e36e
Serg0501 <gavrilenko87sd@gmail.com>
2022-06-06 17:44:58 +0300

    set, map

--- js/main.js
+++ js/main.js
@@ -1,98 +1,65 @@
-//Array. String
-//1 Отфильтруйте отрицательные элементы в массиве, оставив только положительные
-
-//let arr = [ 107, 21, -0.5, 89, 5, 101, - 15, 0.64, 7, 0, -3, 99, 568, 0.1, 55];
-
 /*
-let task1 = arr.filter(el => el>=0);
-console.log(task1);
-*/
-
-//2 Найдите сумму элементов массива которые меньше 100 по значению
-
-/*
-let resLessHund = arr.filter(isLessHund = (value) => value<100);
-let resSum = resLessHund.reduce(sumNum = (a, b) => a + b)
-console.log(resSum);
-*/
-
-/*
-let task2 = arr.filter((el, i) => (el<100)).reduce((accum, val) => accum + val);
-console.log(task2);
-*/
-
-//3 Уменьшите каждый элемент массива на 3%
-
-/*
-let task3 = arr.forEach((el,i,arr)=>arr[i]*=0.97);
-console.log(arr);
-*/
-
-//4 Если все элементы массива меньше 100 увеличьте их на 5%
-
-/*
-//let arr = [ 107, 21, -0.5, 89, 5, 101, - 15, 0.64, 7, 0, -3, 99, 568, 0.1, 55];
-
-//let arr = [ 10, 21, -0.5, 89, 5, 11, - 15, 0.64, 7, 0, -3, 99, 58, 0.1, 55];
-
-let SubHund
-let task4 = arr.every((el,i,arr)=>arr[i]<100)
-? ( SubHund  = arr.map((el,i,arr)=>arr[i]*=1.05),console.log(SubHund))
-:console.log('Not Sub Hundred'); 
-*/
-
-//5 Проверьте является ли слово палиндромом (например слово abcddcba подходит т.к. оно "зеркальное")
-
-/*
-let a1='12345678987654321';
-let a2='1234567890';
-
-function task5 (arr) {
-arr === arr.split('').reverse().join('') // == тоже отрабатывает
-? console.log('Палиндромом')
-: console.log('Не палиндромом')
+let a1 = [1,2,100,200,3];
+function array(c){
+	for (let i = 0; i < c.length; i++){
+	if (c[i] < 100) {
+		c[i]++;
+	}
+	}
 }
-task5 (a1),task5 (a2);
-*/
-
-//6 Соедините две строки в одну и переверните наоборот
-/*
-let a1 = [1,2,3,4,5,6,7,8,9];
-let a2= [10,20,30,40,50,60,70,80,90];
-
-console.log(a1,a2);
-let a3 = a1.concat(a2);
-console.log(a3);
-let a4 = a3.reverse();
-console.log(a4);
-// отрабатывает лучше
-*/
-
-/*
-let a1 = '1,2,3,4,5,6,7,8,9';
-let a2 = '10,20,30,40,50,60,70,80,90'
-
-let task6 = a1.split(' ').concat(a2.split(' ')).reverse().join(' ');
-console.log(task6);
-//отрабатывает не так, как выше!!!!
-*/
-
-//7* Напишите функцию, которая преобразует первую букву каждого слова строки в верхний регистр.
-
-
-//8* Вывести индексы минимальных элементов массива
-/*
-let arr = [ 107, 21, -0.5, 89, 5, 101, - 15, 0.64, 7, 0, -3, 99, 568, 0.1, 55];
-console.log(arr);
-
-function task8(arr, min) {
-    let minEl=[];
-    arr.forEach((el, i) => {
-        if (el<min) {
-            minEl.push(i);
-        }
-    })
-    return minEl;
+console.log(array(c)); //[2,3,100,200,4]
+function array(c){
+    let sum = 0;
+	for (let i = 0; i < c.length; i++){
+	if (c[i] < 100) {
+		sum += c[i];
+	}
+	}
+    return sum;
 }
-console.log(task8(arr, prompt('Введите min:')));
+let res = array(a1);
+console.log(res);
+*/
+
+/*
+let array = [1,2,100,200,3,3,3,3];
+const [a0, ...rest] = array;
+console.log(a0);
+console.log(rest);
+const [arr0, arr1, ,, ...rest2] = array;
+console.log(arr0);
+console.log(arr1);
+console.log(rest2);
+let b = [1,2,3,4, [10,20,[100,200,300],30,40], 5, 6];
+const [,,,,[,,innerArray,,],,] =b
+//const [,,,,[,,innerArray,...r1],...r2] =b
+console.log(innerArray);
+*/
+
+/*
+SET
+набор уникальных значений / похоже на массив только уникальные
+const s1 = new Set();
+const s2 = new Set(array);
+s1.add(100);
+s1.add(-5);
+s1.add('hello');
+s1.delete('hello');
+s1.has(100);
+s1.clear(); //delete all
+*/
+
+//MAP
+// key+value
+/*
+const map1 = new Map();
+map1.set(100,'Vasya');
+map1.set(101,'Ann');
+map1.set(103,'Tom');
+map1.set(104,'Fox');
+map1.delete(103)
+map1.has(100) //true
+map1.get(100) //'Vasya'
+map1.set(100,'Kolya');
+console.log(map1);
 */
