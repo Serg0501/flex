12190b04611a2cb4d30b71122d9183e4f6c26c0c
Serg0501 <gavrilenko87sd@gmail.com>
2022-06-28 14:30:58 +0300

    3d slider

--- js/main.js
+++ js/main.js
@@ -1,140 +1,74 @@
 /*
-1. Создайте 10 параграфов и первым трем дайте класс Выделенный текст, 
-который выделяет текст например фоном.
+const divArr = document.querySelectorAll('.sliderImg');
+
+const l = document.querySelectorAll('sliderImg');
+const b = document.querySelectorAll('sliderBigImg');
+
+function bigImg (e) {
+    
+    this.style.
+    this.removeEventListener('click', bigImg);
+    this.addEventListener('clik', littleImg);
+}
+
+function littleImg (e) {
+
+    this.style.background = 'white';
+    this.removeEventListener('click', littleImg);
+    this.addEventListener('click', bigImg);
+
+}
+
+divArr.forEach( el => el.addEventListener('click', bigImg));
 */
 
-let testP = document.createElement('p');
-testP.className = 'highlightedText';
-testP.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing.';
-document.body.append(testP);
-
-let testP1 = document.createElement('p');
-testP1.className = 'highlightedText';
-testP1.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing.';
-document.body.append(testP1);
-
-let testP2 = document.createElement('p');
-testP2.className = 'highlightedText';
-testP2.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing.';
-document.body.append(testP2);
-
-let testP3 = document.createElement('p');
-testP3.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing.';
-document.body.append(testP3);
-
-let testP4 = document.createElement('p');
-testP4.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing.';
-document.body.append(testP4);
-
-let testP5 = document.createElement('p');
-testP5.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing.';
-document.body.append(testP5);
-
-let textDecor = document.querySelectorAll('p.highlightedText');
-
-textDecor.forEach((el) => {
-   el.style.background = 'blue',el.style.color = 'yellow'; 
-});
-
-//2. Создайте список. В каждый элемент списка напишите текст свой, например дни недели или цвета радуги.
-//2* выделите каждый элемент списка случайным цветом поменяв ему цвет текста или фон
-
-
-function liContent() {
-    let result = [];
-    let arrDeys = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sanday'];
-    let arrColors =['blue','yellow','green','black','silver','gold','aqua'];
-    for(let i=1; i<=7; i++) {
-    let li = document.createElement('li');
-    li.append(i);
-    result.push(li);
-  }
-  result.forEach((el, i) => {
-      el.innerText=arrDeys[i],el.style.color=arrColors[i]
-      /*
-    el.style.background = 'grey';
-    if (i==0) el.innerText = 'Monday', el.style.color = 'blue';
-    if (i==1) el.innerText = 'Tuesday', el.style.color = 'yellow';
-    if (i==2) el.innerText = 'Wednesday', el.style.color = 'green';
-    if (i==3) el.innerText = 'Thursday', el.style.color = 'black';
-    if (i==4) el.innerText = 'Friday', el.style.color = 'silver';
-    if (i==5) el.innerText = 'Saturday', el.style.color = 'gold';
-    if (i==6) el.innerText = 'Sanday', el.style.color = 'aqua';
-    */
-  })
-  return result;
-}
-task2.append(...liContent());
 
 /*
-3* по просьбе трудящихся :)
-объект Товара (название, цена, другие характеристики ...) создайте карточку
+const r = /[a-z]+/g;
+const s = 'Hello Java script regExp 123 !';
+
+console.log(r.test(s));
+// test вернет true/false
+
+//поиск
+//1 match / matchAll
+//2 search
+
+console.log(s.search(r));
+
+console.log(s.match(r));
+*/
+/*
+const regColor = /#[0-9A-F]{6}/g;
+const arr = ['#112233', '#FA34B2', 'BB1122', '#ab1122', '112233'];
+
+for(let i=0; i<arr.length; i++){
+    console.log(regColor.test(arr[i]));
+}
+*/
+/*
+let container = document.getElementById('container');
+const count = 8;
+for(let i=1; i<=count; i++){
+    const slide = document.createElement('div');
+    slide.classList.add('image');
+    container.append(slide);
+    function clickOn() {
+      slide.style.width = '250px';
+   };
+    slide.addEventListener('click', clickOn);
+
+    slide.addEventListener('mouseleave', () => {
+        slide.style.width = '50px';
+    } )
+}
 */
 
-const product = {
-    img: '#',
-    title: 'Pasta',
-    description: {
-        weight:'weight: 500gr',
-        price:'price: 0.85 euro',
-        producing:'producing country: Italy',
-    }
-};
+let box = document.querySelector('.container');
+let sides = document.querySelectorAll('.box');
 
-function createProductImage(productImageSrc){
-    const imgCont = document.createElement('div');
-    const imgProduct = document.createElement('img');
-    imgProduct.setAttribute('src', productImageSrc);
-    imgProduct.setAttribute('alt', 'photo');
-    imgCont.append(imgProduct);
-    return imgCont;
-};
-
-function createTitleProduct(title){
-    const cardTitleProduct = document.createElement('p');
-    cardTitleProduct.classList.add('prodTitle');
-    cardTitleProduct.innerText = title;
-    return cardTitleProduct;
-}
-
-function createDescriptionCont(descriptions){
-    const descriptionCont = document.createElement('div');
-    descriptionCont.classList.add('descrCont');
-    descriptionCont.innerText = 'description:';
-//weight
-    const description1 = document.createElement('p');
-    description1.classList.add('weight');
-    description1.innerText = product.description.weight;
-    descriptionCont.append(description1);
-//price
-    const description2 = document.createElement('p');
-    description2.classList.add('descrCont','price');
-    description2.innerText = product.description.price;
-    descriptionCont.append(description2);
-//producing
-    const description3 = document.createElement('p');
-    description3.classList.add('descrCont','producing');
-    description3.innerText = product.description.producing;
-    descriptionCont.append(description3);
-
-    return descriptionCont;
-};
-
-function createCard (objectCard){
-    const card = document.createElement('div');
-    card.classList.add('cardCont');
-    card.append(createProductImage(objectCard.imgSrc));
-
-    const cardInformation = document.createElement('div');
-    cardInformation.classList.add('infoCont');
-    cardInformation.append(createTitleProduct(objectCard.title));
-    cardInformation.append(createDescriptionCont(objectCard.descriptions));
-
-    card.append(cardInformation);
-    return card;
-};
-
-const prodCard = document.getElementById('productCardCont');
-prodCard.append(createCard(product));
-
-
+sides.forEach((side, i) => {
+    side.addEventListener('click', () => {
+        box.style.transform = `rotateX(-15deg) rotateY(-${i*60}deg)`;
+    })
+});
